0e2c6850f29c0b36a87f5e37b83667f1f7ac3233
Piotr Bielski <piotrbielski3@gmail.com>
2023-06-26 20:03:56 +0100

    Refactor: Template list page; HTTP API calls centralized

---
+++ src/api/tmdb-api.js
@@ -0,0 +1,22 @@
+export const getMovies = () => {
+	return fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${import.meta.env.VITE_TMDB_KEY}&language=en-US&include_adult=false&page=1`)
+		.then(res => res.json())
+		.then(json => json.results);
+};
+
+export const getMovie = ID => {
+	return fetch(`https://api.themoviedb.org/3/movie/${ID}?api_key=${import.meta.env.VITE_TMDB_KEY}`)
+		.then(res => res.json());
+};
+
+export const getGenres = () => {
+	return fetch("https://api.themoviedb.org/3/genre/movie/list?api_key="+import.meta.env.VITE_TMDB_KEY+"&language=en-US")
+		.then(res => res.json())
+		.then(json => json.genres);
+};
+
+export const getMovieImages = ID => {
+	return fetch(`https://api.themoviedb.org/3/movie/${ID}/images?api_key=${import.meta.env.VITE_TMDB_KEY}`)
+		.then(res => res.json())
+		.then(json => json.posters);
+};
