f3fe37c2cd8f88502e12ca2454092c5b24db40e7
Piotr Bielski <piotrbielski3@gmail.com>
2023-06-27 19:26:45 +0100

    Change site header

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,17 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Navigate, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import MoviePage from "./pages/movieDetailsPage";
 import HomePage from "./pages/homePage";
 import FavouriteMoviesPage from "./pages/favouriteMoviesPage.jsx";
 import MovieReviewPage from "./pages/movieReviewPage";
+import SiteHeader from "./components/siteHeader";
 
 const App = () => {
   return(
     <BrowserRouter>
-	    <ul>
-		    <li><Link to="/">Home</Link></li>
-		    <li><Link to="/movies/favourites">Favourites</Link></li>
-	    </ul>
-      <Routes>
+			<SiteHeader />
+	    <Routes>
 	      <Route 
 		      path="/movies/favourites"
 		      element={ <FavouriteMoviesPage /> } 
