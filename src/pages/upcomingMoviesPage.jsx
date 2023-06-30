a0a3f4c84e169c5248037f2d064178f4275fef63
Piotr Bielski <piotrbielski3@gmail.com>
2023-06-30 20:53:11 +0100

    Fix upcoming movies page

--- src/pages/upcomingMoviesPage.jsx
+++ src/pages/upcomingMoviesPage.jsx
@@ -1,32 +1,25 @@
-import React, { useState, useEffect} from "react";
+import React from "react";
 import PageTemplate from "../components/templateMovieListPage";
 import { getUpcomingMovies } from "../api/tmdb-api";
+import {useQuery} from "react-query";
+import Spinner from "../components/spinner";
+import AddToFavouritesIcon from "../components/cardIcons/addToFavourites";
 
-const HomePage = (props) => {
-	const [movies, setMovies] = useState([]);
-	const favourites = movies.filter(m => m.favourite);
-	localStorage.setItem('favourites', JSON.stringify(favourites));
-
-	const addToFavourites = (movieID) => {
-		const updatedMovies = movies.map((m) =>
-			m.id === movieID ? { ...m, favourite: true } : m);
-		setMovies(updatedMovies);
-	};
-
-	useEffect(() => {
-		getUpcomingMovies().then(movies => {
-			setMovies(movies);
-		});
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
+const upcomingMoviesPage = () => {
+	const {data, error, isLoading, isError} = useQuery("upcoming", getUpcomingMovies);
+	if (isLoading) {return <Spinner />;}
+	if (isError) {return <h1>{error.message}</h1>;}
+	const movies = data ? data.results : [];
 
 	return(
 		<PageTemplate
 			title='Upcoming Movies'
 			movies={movies}
-			selectFavourite={addToFavourites}
+			action={(movie) => {
+        return <AddToFavouritesIcon movie={movie} />
+      }}
 		/>
 	)
 };
 
-export default HomePage;
+export default upcomingMoviesPage;
